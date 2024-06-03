5e09b58e5c2d7a9f5db54c6a48013f43a977b229
TuNombreDeUsuario <pabloacisera2021@gmail.com>
2024-06-03 11:36:15 -0300

    testeo del crud- resultado positivo

--- src/user_module/user.controller.ts
+++ src/user_module/user.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Request, Response, Res } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
 import { UserModuleService } from './user_module.service';
-import { User } from './interface/user.interface';
+import { User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly UserModuleService: UserModuleService) {}
+  constructor(private readonly userModuleService: UserModuleService) {}
 
   @Post()
-  async createUser( @Body() userBody:User ) {
-    
-    let userCreated = await this.UserModuleService.createUser(userBody)
-    return userCreated;
+  async createUser(@Body() data: User) {
+    return this.userModuleService.createUser(data);
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) { // @Param('id') para obtener el parámetro id correctamente
+    return this.userModuleService.getUserById(Number(id)); // Number(id) transforma el str en number
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id') id: string) { // @Param('id') para obtener el parámetro id correctamente
+    return this.userModuleService.deleteUser(Number(id));
+  }
+
+  @Put(':id')
+  async updateUserById(@Param('id') id: string, @Body() data: User) { // @Param('id') para obtener el parámetro id correctamente
+    return this.userModuleService.updateUser(Number(id), data);
   }
 }
--- src/user_module/user_module.service.ts
+++ src/user_module/user_module.service.ts
@@ -1,22 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './interface/user.interface';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class UserModuleService {
-  
-  getUserId() {
-    
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUserById(id: number): Promise<User> {
+    const userFound = await this.prisma.user.findUnique({
+      where: { id: id },
+    });
+
+    if (!userFound) {
+      throw new NotFoundException({
+        message: 'usuario no encontrado',
+      });
+    }
+
+    return userFound;
   }
 
-  createUser(userBody: any ){
-    
+  async createUser(data: User): Promise<User> {
+    const newUser = await this.prisma.user.create({ data });
+    return newUser;
   }
 
-  updateUser() {
-    
+  async updateUser(id: number, data: User): Promise<User> {
+    try {
+      const userUpdate = await this.prisma.user.update({
+        where: { id: id },
+        data: data,
+      });
+
+      if (!userUpdate) {
+        throw new NotFoundException({
+          message: 'usuario no encontrado para actualizar',
+        });
+      }
+
+      return userUpdate;
+    } catch (error) {
+      throw new NotFoundException({
+        message: 'Error actualizando el usuario',
+      });
+    }
   }
 
-  deleteUser() {
-   
+  async deleteUser(id: number): Promise<User> {
+    try {
+      const userFound = await this.prisma.user.delete({
+        where: { id: id },
+      });
+
+      return userFound;
+    } catch (error) {
+      throw new NotFoundException({
+        message: 'No se ha podido eliminar el usuario',
+      });
+    }
   }
 }
